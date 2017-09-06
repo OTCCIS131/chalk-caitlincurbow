f9460e9c1c4fa194961f4cb5c1b2043550cff8e7
Caitlin Curbow <cc0722665@otc.edu>
2017-09-06 13:15:03 -0500

    Goose's Birthday

---
+++ index.js
@@ -0,0 +1,14 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.white.bgCyan.italic('Happy ') + 
+chalk.yellow('Birthday ') + 'to ' + 
+chalk.green.bold('YOU') + chalk.magenta('!'));
+log(chalk.cyan('HAPPY ') + chalk.bgMagenta.yellow('BIRTHDAY ') +
+chalk.redBright('to YOUUUUUU') + chalk.blue('!!'));
+log(chalk.bgBlack.magenta('HAPPY BIRTHDAY DEAR ') + 
+chalk.bgYellowBright.black('GOOOOOSSSSSEEEEEE') +
+chalk.greenBright('!!!!!!!!!!!!!!!!!!!!'));
+log(chalk.bgGreen.gray('Haaaaaappy ') + chalk.redBright('Biiiiirrrrrrttttthhhhhhday ') +
+chalk.bgRed.whiteBright('TOOOOOOOO ') + chalk.blue('YOOOOOOOOUUUUUUUUUU')
++ chalk.yellowBright('!!!!!!!!!!!!!!!!!!!'));
